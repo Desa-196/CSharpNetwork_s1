3a3361b2464f42f96f5303eaa18d4cccb669dcaf
Пономарев Денис <desa-196@yandex.ru>
2024-03-31 19:51:41 +0300

    Создал проекты по семинару

---
+++ Client/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using Client;
+
+
+UdpClient udpClient = new UdpClient();
+IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+
+
+while (true)
+{
+    string? messageText;
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("Введите сообщение: ");
+        messageText = Console.ReadLine();
+    }
+    while (string.IsNullOrEmpty(messageText));
+
+    Message message = new Message() { Text = messageText, DateTime = DateTime.Now, NickNameFrom = "Denis", NickNameTo = "Server" };
+    string json = message.SerializeMessageToJson();
+
+    byte[] data = Encoding.UTF8.GetBytes(json);
+    udpClient.Send(data, data.Length, ipEndPoint);
+}
